2739c0d1363efba45c4e49835c0fad2fc18bb031
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 11:28:01 +0200

    methodes classe Forme

--- src/exoNoteCnam/Forme.java
+++ src/exoNoteCnam/Forme.java
@@ -5,7 +5,7 @@ public class Forme {
 	private int surface;
 	private String couleur;
 	
-	//constructeurs
+	//constructeurs vide et initialisé
 	public Forme(){
 		surface =0;
 		couleur = "";
@@ -14,4 +14,34 @@ public class Forme {
 		this.surface= surface;
 		this.couleur= couleur;
 	}
+	// accesseurs et mutateurs
+	public int getSurface(){
+		return surface;
+	}
+	public void setSurface(int surface){
+		this.surface = surface;
+	}
+	
+	public String getCouleur(){
+		return couleur;
+	}
+	public void setCouleur(String couleur){
+		this.couleur = couleur;
+	}
+		//méthodes
+	public String afficher(){
+		String af = "Forme affichée";
+		return af;
+	}
+	public String masquer(){
+		String ma = "Forme masquée";
+		return ma;
+	}
+	public String description(){
+		String des = "La forme de surface "+this.getSurface()+" est de couleur "+this.getCouleur();
+		return des;
+	}
+
 }
+
+
