6e18e78a72146db8aa0e21fb15dd5809b9dd71e3
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 11:02:50 +0200

    création de la Classe Foerme propriétés et constructeurs

---
+++ src/exoNoteCnam/Forme.java
@@ -0,0 +1,17 @@
+package exoNoteCnam;
+
+public class Forme {
+	//propriétés
+	private int surface;
+	private String couleur;
+	
+	//constructeurs
+	public Forme(){
+		surface =0;
+		couleur = "";
+	}
+	public Forme(int surface, String couleur){
+		this.surface= surface;
+		this.couleur= couleur;
+	}
+}
