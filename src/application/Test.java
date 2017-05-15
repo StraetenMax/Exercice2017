3329043eb4d256eb2e9d49daa45d6c2e0e462da2
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 13:03:50 +0200

    correction classe Test

--- src/application/Test.java
+++ src/application/Test.java
@@ -6,16 +6,18 @@ import exoNoteCnam.Forme;
 public class Test {
 
 	public static void main(String[] args) {
-		Forme[] formes = new Forme[3];
+		Forme[] formes = new Forme[2];
 		Scanner clavier = new Scanner(System.in);
 		for(int i=0; i<formes.length; i++){
-			System.out.print("Donner à "+(i+1)+" une surface ");
-			int surface = clavier.nextInt();
-			System.out.print("Donner à "+(i+1)+" une couleur ");
-			String couleur = clavier.nextLine();
-			formes[i]= new Forme(surface, couleur);
+			System.out.println("Donner à "+(i+1)+" une surface ");
+			int surf = clavier.nextInt();
+			System.out.println("Donner à "+(i+1)+" une couleur ");
+			String coul = clavier.nextLine();
+			formes[i]= new Forme(surf, coul);
 		}
 		clavier.close();
+		
+		
 		
 	}
 
