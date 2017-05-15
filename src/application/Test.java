44dd9b8a2fdbb66523273ee412927b2e6e212b2a
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 13:10:39 +0200

    modification classe Test

--- src/application/Test.java
+++ src/application/Test.java
@@ -1,12 +1,12 @@
 package application;
 
-import java.util.Scanner;
+//import java.util.Scanner;
 import exoNoteCnam.Forme;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Forme[] formes = new Forme[2];
+		/*Forme[] formes = new Forme[2];
 		Scanner clavier = new Scanner(System.in);
 		for(int i=0; i<formes.length; i++){
 			System.out.println("Donner à "+(i+1)+" une surface ");
@@ -15,7 +15,10 @@ public class Test {
 			String coul = clavier.nextLine();
 			formes[i]= new Forme(surf, coul);
 		}
-		clavier.close();
+		clavier.close();*/
+		Forme F1 = new Forme(200, "Rouge");
+		Forme F2 = new Forme(250, "Jaune");
+		Forme F3 = new Forme(200, "Vert");
 		
 		
 		
