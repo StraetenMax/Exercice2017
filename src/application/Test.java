f7b6aff8cd947956d9a6ef44be26c90583cd437d
StraetenMax <maxstraeten@yahoo.com>
2017-05-15 11:41:10 +0200

    réalisation du test

---
+++ src/application/Test.java
@@ -0,0 +1,20 @@
+package application;
+
+import java.util.Scanner;
+import exoNoteCnam.Forme;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Forme[] formes = new Forme[3];
+		Scanner clavier = new Scanner(System.in);
+		for(int i=0; i<formes.length; i++){
+			System.out.print("Donner à "+(i+1)+" une surface ");
+			int surface = clavier.nextInt();
+			System.out.print("Donner à "+(i+1)+" une couleur ");
+			String couleur = clavier.nextLine();
+		}
+		//formes[i]=(surface, couleur);
+	}
+
+}
