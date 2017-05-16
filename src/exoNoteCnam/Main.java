3268543bf38997ab9c7f586cd20c3f954eea280b
StraetenMax <maxstraeten@yahoo.com>
2017-05-16 10:15:50 +0200

    appeler la méthode description()

--- src/exoNoteCnam/Main.java
+++ src/exoNoteCnam/Main.java
@@ -7,7 +7,16 @@ public class Main {
 		Forme F1 = new Forme(123,"Blanc");
 		Quadrilatere Q1 = new Quadrilatere(145,"Bleu","Concave");
 		Carre C1 = new Carre(127, "Vert","Convexe",4);
-		Carre C2 = new Carre();
+		Carre C2 = new Carre(169, "Rouge","Croisé",6);
+		
+		Rectangle R1 = new Rectangle(115,"Bleu","Convexe",18);
+		Rectangle R2 = new Rectangle(189, "Gris","Concave",22);
+	
+		//appeler les méthodes
+		System.out.println(F1.description());
+		System.out.println(Q1.description());
+		System.out.println(R1.description());
+	
 	}
 
 }
