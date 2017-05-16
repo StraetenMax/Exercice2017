6037318e118574c9c8532c8d92d939b29fccd532
StraetenMax <maxstraeten@yahoo.com>
2017-05-16 08:02:14 +0200

    Carre constructeur

--- src/exoNoteCnam/Carre.java
+++ src/exoNoteCnam/Carre.java
@@ -2,5 +2,20 @@ package exoNoteCnam;
 
 public class Carre extends Quadrilatere{
 	//propriétés
+	private int tailleCote;
+	private boolean grand;
+	
+	//constructeurs vide initialisé
+	public Carre(){
+		super();
+		tailleCote= 0;
+		//this.getGrand();
+	}
+	public Carre(int surface, String couleur,String type,int tailleCote){
+		super(surface, couleur, type);
+		this.tailleCote = tailleCote;
+		//this.getGrand();
+	}
+	
 	
 }
