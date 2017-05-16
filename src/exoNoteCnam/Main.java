5a35e9299114a2e45e41fad434c5e16a6540959c
StraetenMax <maxstraeten@yahoo.com>
2017-05-16 10:10:28 +0200

    classe Main instanciers les classes

---
+++ src/exoNoteCnam/Main.java
@@ -0,0 +1,13 @@
+package exoNoteCnam;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//instanciers les classes
+		Forme F1 = new Forme(123,"Blanc");
+		Quadrilatere Q1 = new Quadrilatere(145,"Bleu","Concave");
+		Carre C1 = new Carre(127, "Vert","Convexe",4);
+		Carre C2 = new Carre();
+	}
+
+}
